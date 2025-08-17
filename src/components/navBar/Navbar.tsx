44c3ffbed7f11fdf6c4782c7b2037b7cba05e6f4
ItaloHespanholPeres <pereshespanhol1@gmail.com>
2025-08-16 22:01:17 -0300

    O Básico

---
+++ src/components/navBar/Navbar.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import "./NavBar.css";
+
+export default function NavBar() {
+  const [manga, setManga] = useState("");
+
+  return (
+    <nav className="NavBar">
+      <h1>Project Rice</h1>
+      <div className="Search">
+        <input
+          type="text"
+          className="InputText"
+          onChange={(e) => setManga(e.target.value)}
+        />
+        <button className="Submit" onClick={() => console.log(manga)}>
+          Pesquisar
+        </button>
+      </div>
+    </nav>
+  );
+}
