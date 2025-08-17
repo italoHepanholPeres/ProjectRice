3ddfdb2760cfc03fafef3f4b40b255cde3bb9bd4
ItaloHespanholPeres <pereshespanhol1@gmail.com>
2025-08-17 02:39:53 -0300

    organização 2

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import "./NavBar.css";
+import "./NavBarStyle.css";
 
 export default function NavBar() {
   const [manga, setManga] = useState("");
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,17 @@
-import "./";
-import NavBar from "../../components/navBar/Navbar";
+import "./style.css";
+import NavBar from "../../components/navBar/NavBar";
 
 function App() {
-  return <NavBar />;
+  return (
+    <div>
+      <NavBar />
+      <div>
+        <h1>teste</h1>
+        <h1>teste</h1>
+        <h1>teste</h1>
+      </div>
+    </div>
+  );
 }
 
 export default App;
