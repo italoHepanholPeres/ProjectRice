4a30724d023acbb10844ec007fa34def9c36464c
ItaloHespanholPeres <pereshespanhol1@gmail.com>
2025-08-21 17:27:26 -0300

    gardiante adicionado

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,7 +2,7 @@ import NavBar from "../../components/navBar/NavBar";
 
 function Home() {
   return (
-    <div>
+    <div className="flex h-screen w-full flex-col bg-purple-400 bg-gradient-to-b from-purple-700 to-purple-500">
       <NavBar />
       <div>
         Lidos recentemente:
